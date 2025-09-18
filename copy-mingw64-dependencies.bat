886bd281d31dd8cb59689b612e51853bc9eb0549
Benilda Key <BKey76@GMail.com>
2025-09-18 11:45:22 -0500

    Added copy-mingw64-dependencies.bat.

---
+++ copy-mingw64-dependencies.bat
@@ -0,0 +1,46 @@
+@echo off
+setlocal
+call :GetBatchFileDirectory _MyDir
+set BIN_DIR=%_MyDir%\build\bin
+
+copy /Y "C:\mingw64\bin\libstdc++-6.dll" "%BIN_DIR%"
+copy /Y "C:\mingw64\bin\libgcc_s_seh-1.dll" "%BIN_DIR%"
+
+
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libjpeg-62.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\liblzma.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libpcre2-16.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libpng16.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libsharpyuv.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libtiff.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libwebp.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libwebpdecoder.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libwebpdemux.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libzlib1.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libzstd.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxbase331u_gcc_custom.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxmsw331u_core_gcc_custom.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxmsw331u_html_gcc_custom.dll" "%BIN_DIR%"
+
+goto :EOF
+
+::
+:: GetBatchFileDirectory
+::
+:: Gets the name of the directory in which the batch file is located.  The directory name will not
+:: have a final trailing \ character.
+::
+:: The directory name is stored in the environment variable specified by the first parameter of the
+:: function.
+::
+:GetBatchFileDirectory
+  set _dir=%~dp0
+  set _dir=%_dir:~0,-1%
+  if "%_dir%" EQU "" (
+    set _dir=
+    exit /b 1
+  )
+  set %1=%_dir%
+  set _dir=
+  exit /b 0
+goto :EOF
