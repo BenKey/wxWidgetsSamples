41d86e59c214dd5fd66c87211b0792aba509eb0e
Benilda Key <BKey76@GMail.com>
2025-09-18 22:15:16 -0500

    Updated copy-mingw64-dependencies.bat.

--- copy-mingw64-dependencies.bat
+++ copy-mingw64-dependencies.bat
@@ -6,7 +6,7 @@ set BIN_DIR=%_MyDir%\build\bin
 copy /Y "C:\mingw64\bin\libstdc++-6.dll" "%BIN_DIR%"
 copy /Y "C:\mingw64\bin\libgcc_s_seh-1.dll" "%BIN_DIR%"
 
-
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libexpat-1.dll" "%BIN_DIR%"
 copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libjpeg-62.dll" "%BIN_DIR%"
 copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\liblzma.dll" "%BIN_DIR%"
 copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libpcre2-16.dll" "%BIN_DIR%"
@@ -19,8 +19,18 @@ copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libwebpdemux.dll" 
 copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libzlib1.dll" "%BIN_DIR%"
 copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\libzstd.dll" "%BIN_DIR%"
 copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxbase331u_gcc_custom.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxbase331u_net_gcc_custom.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxbase331u_xml_gcc_custom.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxmsw331u_aui_gcc_custom.dll" "%BIN_DIR%"
 copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxmsw331u_core_gcc_custom.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxmsw331u_gl_gcc_custom.dll" "%BIN_DIR%"
 copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxmsw331u_html_gcc_custom.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxmsw331u_media_gcc_custom.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxmsw331u_propgrid_gcc_custom.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxmsw331u_ribbon_gcc_custom.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxmsw331u_richtext_gcc_custom.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxmsw331u_stc_gcc_custom.dll" "%BIN_DIR%"
+copy /Y "%VCPKG_ROOT_MINGW64%\installed\x64-mingw-dynamic\bin\wxmsw331u_xrc_gcc_custom.dll" "%BIN_DIR%"
 
 goto :EOF
 
